df9c88c3373438ff86bd7633eae06918b54a7770
TristanTm <33825@ma-web.nl>
2021-11-30 12:37:49 +0100

    Werkende koelkast

--- web/les2-boodschappen/script.js
+++ web/les2-boodschappen/script.js
@@ -1,1 +1,22 @@
 console.log("Script is geladen");
+
+const groentevak = document.getElementById('groente');
+const fruitvak = document.getElementById('fruit');
+
+
+function zetInFruitLa(fruit){
+    console.log(fruit);
+
+    const nieuwElement = document.createElement('h2');
+    nieuwElement.innerHTML = fruit;
+    fruitvak.appendChild(nieuwElement);
+   
+}
+
+function zetInGroenteLa(groente){
+    console.log(groente);
+
+    const nieuwElement = document.createElement('h2');
+    nieuwElement.innerHTML = groente;
+    groentevak.appendChild(nieuwElement);
+}
